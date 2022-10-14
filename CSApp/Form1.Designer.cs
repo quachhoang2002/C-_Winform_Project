a3ce1343fe85447054896d7e10a297ef75575c9f
onhauyennhi <onhauyennhi254@gmail.com>
2022-10-14 10:34:41 +0700

    fix conflict

--- CSApp/Form1.Designer.cs
+++ CSApp/Form1.Designer.cs
@@ -30,30 +30,6 @@
         {
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
-<<<<<<< HEAD:WinFormsApp1/Form1.Designer.cs
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button2 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(334, 173);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 33);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(630, 242);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(121, 29);
-            this.checkBox1.TabIndex = 2;
-=======
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -64,47 +40,27 @@
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 0;
->>>>>>> ad4cf2e76b0d6a4ec935b49d8b6c1203bbdaeb61:CSApp/Form1.Designer.cs
             this.checkBox1.Text = "checkBox1";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-<<<<<<< HEAD:WinFormsApp1/Form1.Designer.cs
-            this.checkBox2.Location = new System.Drawing.Point(711, 170);
-            this.checkBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(121, 29);
-            this.checkBox2.TabIndex = 3;
-=======
             this.checkBox2.Location = new System.Drawing.Point(301, 144);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(80, 17);
             this.checkBox2.TabIndex = 1;
->>>>>>> ad4cf2e76b0d6a4ec935b49d8b6c1203bbdaeb61:CSApp/Form1.Designer.cs
             this.checkBox2.Text = "checkBox2";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-<<<<<<< HEAD:WinFormsApp1/Form1.Designer.cs
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(743, 117);
-            this.checkBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(121, 29);
-            this.checkBox3.TabIndex = 4;
-            this.checkBox3.Text = "checkBox3";
-            this.checkBox3.UseVisualStyleBackColor = true;
-=======
             this.button1.Location = new System.Drawing.Point(124, 190);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
->>>>>>> ad4cf2e76b0d6a4ec935b49d8b6c1203bbdaeb61:CSApp/Form1.Designer.cs
             // 
             // button2
             // 
@@ -117,24 +73,12 @@
             // 
             // Form1
             // 
-<<<<<<< HEAD:WinFormsApp1/Form1.Designer.cs
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1143, 750);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-=======
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
->>>>>>> ad4cf2e76b0d6a4ec935b49d8b6c1203bbdaeb61:CSApp/Form1.Designer.cs
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -144,18 +88,10 @@
 
         #endregion
 
-<<<<<<< HEAD:WinFormsApp1/Form1.Designer.cs
-        private Button button1;
-        private CheckBox checkBox1;
-        private CheckBox checkBox2;
-        private CheckBox checkBox3;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private Button button2;
-=======
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Button button1;
->>>>>>> ad4cf2e76b0d6a4ec935b49d8b6c1203bbdaeb61:CSApp/Form1.Designer.cs
+        private System.Windows.Forms.Button button2;
     }
 }
 
