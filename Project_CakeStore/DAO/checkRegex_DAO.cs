7cea54432fcdea3302e65de7f8b755b19e000506
Tuh <dotuan2k2@gmail.com>
2022-11-04 11:16:33 +0700

    them file checkRegex

---
+++ Project_CakeStore/DAO/checkRegex_DAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Project_CakeStore.DAO
+{
+    internal class checkRegex_DAO
+    {
+        Regex emailRegex = new Regex("(?<user>[^@]+)@(?<host>.+)");
+        Regex validatePhoneNumberRegex = new Regex("^\\+?[1-9][0-9]{7,14}$");
+
+		public bool checkEmail(string Email)
+		{
+			return emailRegex.IsMatch(Email);
+		}
+
+		public bool checkPhoneNumber(string PhoneNumber)
+		{
+			return validatePhoneNumberRegex.IsMatch(PhoneNumber);
+		}
+
+		public bool checkOnlyLetter(string input)
+		{
+			return Regex.IsMatch(input, @"^[a-zA-Z]+$");
+		}
+
+		
+
+	}
+}
