94422aabd6cca32b9c735c4f516b0acfbe6924e6
User12Col <94947283+User12Col@users.noreply.github.com>
2022-11-07 00:14:03 +0700

    loc push code

--- Project_CakeStore/BUS/cake_BUS.cs
+++ Project_CakeStore/BUS/cake_BUS.cs
@@ -36,5 +36,25 @@ namespace Project_CakeStore.BUS
         {
             return cake_DAO.updateQuantityCakePlus(cakeID, quantity);
         }
+
+        public Boolean addCakeAuto(cake_DTO cake)
+        {
+            return cake_DAO.addCakeAuto(cake);
+        }
+
+        public Boolean deleteCake(String cakeID)
+        {
+            return cake_DAO.deleteCake(cakeID);
+        }
+
+        public Boolean editCake(cake_DTO cake)
+        {
+            return cake_DAO.editCake(cake);
+        }
+
+        public List<cake_DTO> searchCake(String column, String data)
+        {
+            return cake_DAO.searchCake(column, data);
+        }
     }
 }
--- Project_CakeStore/BUS/category_BUS.cs
+++ Project_CakeStore/BUS/category_BUS.cs
@@ -16,5 +16,25 @@ namespace Project_CakeStore.BUS
         {
             return cate_DAO.getAllCategory();
         }
+
+        public Boolean addCate(category_DTO cate)
+        {
+            return cate_DAO.addCate(cate);
+        }
+
+        public Boolean deleteCate(String cateID)
+        {
+            return cate_DAO.deleteCate(cateID);
+        }
+
+        public Boolean editCate(category_DTO cate)
+        {
+            return cate_DAO.editCate(cate);
+        }
+
+        public List<category_DTO> searchCate(String column, String data)
+        {
+            return cate_DAO.searchCate(column, data);
+        }
     }
 }
